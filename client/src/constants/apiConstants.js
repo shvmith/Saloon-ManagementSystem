53ddb5a3039e76deb67114a74d5cac81b74400b1
Roshith Sandeepa <rosandeepasamith@gmail.com>
2025-10-08 19:22:39 +0530

    constants file added

---
+++ client/src/constants/apiConstants.js
@@ -0,0 +1,10 @@
+const API_CONSTANTS = {
+  /// authentication
+  SIGNUP: "/api/v1/auth/signup",
+  LOGIN: "/api/v1/auth/login",
+  SOCIAL_AUTH: "/api/v1/auth/social-auth",
+  LOGOUT: "/api/v1/auth/signout",
+  GET_CURRENT_USER: "/api/v1/auth/me",
+};
+
+export default API_CONSTANTS;
