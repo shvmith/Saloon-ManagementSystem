29b6a54cb61f6d6d00655abf8f9d475fc7b44874
Roshith Sandeepa <rosandeepasamith@gmail.com>
2025-10-08 19:22:54 +0530

    apiConfig.js added

---
+++ client/src/config/apiConfig.js
@@ -0,0 +1,16 @@
+
+// api config
+
+const API_CONFIG = {
+  BASE_URL: "http://localhost:7001", // Base URL for your backend
+  ENDPOINTS: {
+   
+    INVENTORY: "/api/v1/inventory", // Added inventory endpoint
+    APPOINTMENTS: "/api/v1/appoiment", // Added appointments endpoint
+    SERVICES: "/api/services",
+    PACK: "/api/v1/package",
+    FEEDBACK: "/api/v1/feedback", // Added feedback endpoint
+  },
+};
+
+export default API_CONFIG;
